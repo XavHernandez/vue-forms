309c68c5331e67c497335471b6c8c08ee7994820
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-08-16 20:06:49 +0200

    keyboard events, submit events and validations

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,16 +1,21 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <label>Email:</label>
     <input type="email" required v-model="email">
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <label>Skills => Add with 'enter'. Click to delete.</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill"><span @click="deleteSkill(skill)">{{ skill }}</span></div>
 
     <div class="terms">
       <input type="checkbox" required v-model="terms">
@@ -20,14 +25,16 @@
     <div>
       <input type="checkbox" value="Tony" v-model="names">
       <label>Tony</label>
-    </div>
-    <div>
+    
       <input type="checkbox" value="Bruce" v-model="names">
       <label>Bruce</label>
-    </div>
-    <div>
+   
       <input type="checkbox"  value="Stephen" v-model="names">
       <label>Stephen</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
   </form>
 
@@ -47,7 +54,31 @@ export default {
      password: '',
      role: '',
      terms: false,
-     names: []
+     names: [],
+     tempSkill: '',
+     skills: [],
+     passwordError: ''
+   }
+ },
+ methods: {
+   addSkill(e) {
+     if (e.key === 'Enter' && this.tempSkill) {
+       this.skills.push(this.tempSkill)
+       this.tempSkill = ''
+     }
+   },
+   deleteSkill(skill) {
+     this.skills = this.skills.filter((item) => {
+       return skill !== item
+     })
+   },
+   submitForm() {
+     this.passwordError = this.passwordError.length > 5 ? '' : 'Password must have at least 6 characters!'
+     if (!this.passwordError) {
+       console.log('email: ', this.email)
+       console.log('password: ', this.password)
+       console.log('skills: ', this.skills)
+     }
    }
  }
 }
@@ -65,7 +96,8 @@ form{
 label{
   color: #aaa;
   display: inline-block;
-  margin: 25px 0 15px;
+  margin: 25px 0px 15px;
+  padding-right: 10px;
   font-size: 0.6em;
   text-transform: uppercase;
   letter-spacing: 1px;
@@ -86,5 +118,34 @@ input[type="checkbox"]{
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill{
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit{
+  text-align: center;
+}
+.error{
+  color: crimson;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
